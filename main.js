b7d5d7ed4f7aa1502dcc6721182d7d184d0694f1
dwikinuridhuha <dwiki.nuridhuha@gmail.com>
2021-09-06 09:21:54 +0700

    diagonalDifference

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
-const ar = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
+const arr = [[1,2,3], [4,5,6], [9,8,9]];
 
-function aVeryBigSum(ar) {
+function diagonalDifference(arr) {
     // Write your code here
-    let bigAr = [];
-    ar.forEach((nilai, index) => {
-        bigAr[index] = BigInt(nilai);
-    });
+    let diagonal_1 = [],
+        diagonal_2 = [],
+        total = 0,
+        total_1 = 0,
+        total_2 = 0,
+        arrIndex = 0,
+        panjangArr = arr.length - 1;
 
-    return bigAr.reduce((total, num) => {
-        return  BigInt(total) + num;
+    for(arrIndex in arr) {
+        diagonal_1[arrIndex] = arr[arrIndex][arrIndex];
+    }
+
+    for(arrIndex in arr) {
+        diagonal_2[arrIndex] = arr[arrIndex][panjangArr - arrIndex];
+    }
+
+    total_1 = diagonal_1.reduce((total, num) => {
+        return total + num;
     }, 0);
+
+    total_2 = diagonal_2.reduce((total, num) => {
+        return total + num;
+    }, 0);
+
+    // console.log(diagonal_1);
+    // console.log(diagonal_2);
+    //
+    // console.log(total_1);
+    // console.log(total_2);
+
+    total = Math.abs(total_1 - total_2);
+
+    return total;
 }
 
-
-console.log(aVeryBigSum(ar));
+console.log(diagonalDifference(arr));
